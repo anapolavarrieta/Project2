e803aaacf4147ae02a6ed99c2f8bda8f8e60ed25
Ana Paula Olavarrieta <anapolavarrieta@gmail.com>
2014-07-06 12:29:52 -0400

    Added error checking

--- logic.php
+++ logic.php
@@ -15,26 +15,34 @@ fclose ($dict);
 
 $num_wds_dict = count ($words_array);
 $password =" ";	
-if (isset($_POST["words"])){
+$num_wds_pass=4;
+$error=0;
+
+if(isset($_POST["words"]) && $_POST["words"] >0){
 	$num_wds_pass = $_POST["words"]; //Gives the number of words in the password by the user
+	//Generates the password
+	for ($i=0; $i < $num_wds_pass; $i++){
+		$rand = rand(0, $num_wds_dict);
+		$password = $password." ".$words_array[$rand];
+	}
 }
 else{
-	$num_wds_pass= 4;
+	$error=1;
+	$password= "YOU HAVE NOT CHOOSEN A VALID NUMBER OF WORDS";
 }
 
-//Generates the password
-for ($i=0; $i < $num_wds_pass; $i++){
-	$rand = rand(0, $num_wds_dict);
-	$password = $password." ".$words_array[$rand];
+//This condition was made, so the first time the page is launched the message is not the error message
+if (!isset($_POST["words"])){
+	$password="CHOOSE NUMBER OF WORDS";
 }
 
 //Adds a number at the end, if requested by user
-if(isset($_POST["number"])){
+if(isset($_POST["number"]) && $error==0){
 	$rand= rand(0,100);
 	$password = $password.$rand;
 }
 
-if(isset($_POST["symbol"])){
+if(isset($_POST["symbol"]) && $error==0){
 	$password = $password."@";
 }
 
